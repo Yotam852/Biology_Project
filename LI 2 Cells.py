7e37dba11641d6747c0df8c5ff124c798db28fc5
Yotam <yotam1612@gmail.com>
2024-04-15 13:04:56 +0300

    רוב הקוד כתוב, בינתיים שני הרכיבים של R ו- D בהתאמה יוצאים זהים, צריך לבדוק מה הבעיה

--- LI 2 Cells.py
+++ LI 2 Cells.py
@@ -9,11 +9,12 @@ from scipy.integrate import odeint
 
 def model(z, t, betaD, betaR, v, n, m):
     D1, D2, R1, R2 = z
-    dD1dt = v * (betaD / (1 + R1**n) - D1)
-    dD2dt = v * (betaD / (1 + R2**n) - D2)
-    dR1dt = betaR * D2**m / (1 + D2**m) - R1
-    dR2dt = betaR * D1**m / (1 + D1**m) - R2
+    dD1dt = v * (betaD / (1 + R1 ** n) - D1)
+    dD2dt = v * (betaD / (1 + R2 ** n) - D2)
+    dR1dt = betaR * D2 ** m / (1 + D2 ** m) - R1
+    dR2dt = betaR * D1 ** m / (1 + D1 ** m) - R2
     return [dD1dt, dD2dt, dR1dt, dR2dt]
+
 
 # setting up the time axis
 
@@ -34,9 +35,7 @@ z0 = [1e-5 * betaD, 1e-5, 0, 0]
 # solving the ODE
 
 z = odeint(model, z0, t, args=(n, m, betaD, betaR, v))
-
-# print(z)
-
+print(z)
 D1 = z[:, 0]
 D2 = z[:, 1]
 R1 = z[:, 2]
@@ -56,8 +55,8 @@ ax[1].plot(t, R1, 'r-')
 ax[1].plot(t, R2, 'b-')
 ax[1].legend(['R1', 'R2'], loc='best')
 
-fig.text(0.5, 0.04, 'time', ha='center')
-fig.text(0.04, 0.5, 'concentration', va='center', rotation='vertical')
+fig.text(0.5, 0.04, 'time [a.u]', ha='center')
+fig.text(0.04, 0.5, 'concentration [a.u]', va='center', rotation='vertical')
 fig.suptitle('Lateral Inhibition Model for 2 Cells')
 
 plt.show()
