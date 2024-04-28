696e720dc773be3252170a37ca751850470d4324
Yotam <yotam1612@gmail.com>
2024-04-28 21:38:41 +0300

    coding the Stability Analysis

--- Stability Analysis.py
+++ Stability Analysis.py
@@ -6,20 +6,27 @@ from scipy.integrate import odeint
 import sympy as smp
 
 # Define the variables
-x, y, b, n, r = smp.symbols('x y b n r')
+
+x, y = smp.symbols('x y')
+beta = 10
+gamma = 1
+n = 1
 
 # Define the functions
-x_s = 1/r * b/(1 + y**n)
-y_s = 1/r * b/(1 + x**n)
+
+x_s = 1/gamma * beta/(1 + y**n)
+y_s = 1/gamma * beta/(1 + x**n)
 
 # Calculate the derivatives
-values = {x: x_s, y: y_s, b: 10, n: 1, r: 1}
+
+values = {x: x_s, y: y_s}
 dx_s_dx = x_s.diff(x).subs(values)
 dx_s_dy = x_s.diff(y).subs(values)
 dy_s_dx = y_s.diff(x).subs(values)
 dy_s_dy = y_s.diff(y).subs(values)
 
 # Display the results
+
 print(dx_s_dx)
 print(dx_s_dy)
 print(dy_s_dx)
